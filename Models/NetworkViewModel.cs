a7e59d7a6c1fbeb5553a4d0a02b3e050857f25e2
akale <akale@visa.com>
2016-09-20 18:13:25 +0530

    missing file push

---
+++ Models/NetworkViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSN.Models
+{
+    public class NetworkViewModel
+    {
+        public List<IndividualViewModel> Individuals { get; set; }
+
+        public List<OrganizationViewModel> Organizations { get; set; }
+    }
+}
